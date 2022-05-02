a89276bcd90234193fd37e9844be48e500a99fdd
MADARINHA <n-arlan12@hotmail.com>
2022-05-02 19:08:14 -0300

    Create teste.c

---
+++ teste.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main(void){
+
+    int resultado = 1;
+    double pontoA;
+    double pontoB;
+    double pontoMedio;
+    double erro = 0.0001;
+
+    resultado = resultado + 1;
+
+    while(resultado != 1){
+
+        pontoMedio = (pontoA + pontoB)/2;
+
+        resultado = verificaPontoMedioEDelta(pontoA, pontoB, pontoMedio, erro);
+
+        if(resultado == 0){
+
+            double funcaoDeA;
+            double funcaoDeB;
+            double funcaoDeM;
+
+            if((funcaoDeA * funcaoDeM) > 0){
+
+                pontoA = pontoMedio;
+
+            }else if((funcaoDeB * funcaoDeM) > 0){
+
+                pontoB = pontoMedio;
+
+            }
+
+
+        }else{
+
+            printf("solucao eh: %lf", pontoMedio);
+
+        }
+
+    }
+
+    return 0;
+
+}
+
+int calculaDelta(double parametroA, double parametroB, double medio, double epsilon){
+
+    double funcaoDeM;
+    double delta;
+
+    funcaoDeM = (atan(medio)) - (1/exp(medio));
+
+    delta = abs(parametroB - parametroA);
+
+    if(funcaoDeM == 0)return 1;
+    if(delta < epsilon)return 1;
+
+    return 0;
+
+}
