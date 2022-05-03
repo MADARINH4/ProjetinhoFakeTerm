efeda0a46847b7912e259a99e7070bf46cee3c68
MADARINHA <n-arlan12@hotmail.com>
2022-05-02 21:46:16 -0300

    primeira parte

--- teste.c
+++ teste.c
@@ -4,16 +4,14 @@
 #include <stdbool.h>
 #include <math.h>
 
-//void metodoBissecao(double parametroA, double parametroB, double medio, double epsilon);
+void metodoBissecao(double parametroA, double parametroB, double epsilon);
 int verificaPontoMedioEDelta(double parametroA, double parametroB, double medio, double epsilon);
 
 int main(void){
 
     double verifica = 1;
-    int resultado = 1;
     double pontoA = 0;
     double pontoB = 1;
-    double pontoMedio;
     double erro = 0.0001;
     double funcaoDeA;
     double funcaoDeB;
@@ -21,52 +19,19 @@ int main(void){
 
     while(verifica > 0){
 
+        system("cls");
+
         printf("\nDigite o intervalo: ");
         scanf("%lf%lf", &pontoA, &pontoB);
 
         funcaoDeA = ((atan(pontoA)) - (1/exp(pontoA)));
         funcaoDeB = ((atan(pontoB)) - (1/exp(pontoB)));
 
-        printf("\n\nverifica eh: %.9lf.......%.9lf", funcaoDeA, funcaoDeB);
-
         verifica = (funcaoDeA * funcaoDeB);
 
-        //printf("\n\nverifica eh: %.9lf", verifica);
-
         if(verifica < 0){
-
-            while(resultado != 1){
-
-                pontoMedio = (pontoA + pontoB)/2;
-
-                resultado = verificaPontoMedioEDelta(pontoA, pontoB, pontoMedio, erro);
-
-                if(resultado == 0){
-
-                    double funcaoDeM;
-
-                    funcaoDeA = ((atan(pontoA)) - (1/exp(pontoA)));
-                    funcaoDeB = ((atan(pontoB)) - (1/exp(pontoB)));
-                    funcaoDeM = ((atan(pontoMedio)) - (1/exp(pontoMedio)));
-
-                    if((funcaoDeA * funcaoDeM) > 0){
-
-                        pontoA = pontoMedio;
-
-                    }else if((funcaoDeB * funcaoDeM) > 0){
-
-                        pontoB = pontoMedio;
-
-                    }
-
-
-                }else{
-
-                    printf("\n\nsolucao eh: %.9lf", pontoMedio);
-
-                }
-
-            }
+            
+            metodoBissecao(pontoA, pontoB, erro);
 
         }
 
@@ -89,5 +54,47 @@ int verificaPontoMedioEDelta(double parametroA, double parametroB, double medio,
     if(delta < epsilon)return 1;
 
     return 0;
+
+}
+
+void metodoBissecao(double parametroA, double parametroB, double epsilon){
+
+    int resultado = 0;
+    double pontoMedio;
+    double funcaoDeA;
+    double funcaoDeB;
+
+    while(resultado != 1){
+
+        pontoMedio = (parametroA + parametroB)/2;
+
+        resultado = verificaPontoMedioEDelta(parametroA, parametroB, pontoMedio, epsilon);
+
+        if(resultado == 0){
+
+            double funcaoDeM;
+
+            funcaoDeA = ((atan(parametroA)) - (1/exp(parametroA)));
+            funcaoDeB = ((atan(parametroB)) - (1/exp(parametroB)));
+            funcaoDeM = ((atan(pontoMedio)) - (1/exp(pontoMedio)));
+
+            if((funcaoDeA * funcaoDeM) > 0){
+
+                parametroA = pontoMedio;
+
+            }else if((funcaoDeB * funcaoDeM) > 0){
+
+                parametroB = pontoMedio;
+
+            }
+
+
+        }else{
+
+            printf("\n\nsolucao eh: %.9lf", pontoMedio);
+
+        }
+
+    }
 
 }
